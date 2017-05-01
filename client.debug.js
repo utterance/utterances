a467a3f5eeb9fe681748c5abdb4fe2534901c698
Jeremy Danyow <jdanyow@gmail.com>
2017-04-30 20:18:06 -0700

    Updates

---
+++ client.debug.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+function param(obj) {
+    var parts = [];
+    for (var name_1 in obj) {
+        if (obj.hasOwnProperty(name_1)) {
+            parts.push(encodeURIComponent(name_1) + "=" + encodeURIComponent(obj[name_1]));
+        }
+    }
+    return parts.join('&');
+}
+
+var script = document.currentScript;
+var attrs = {};
+for (var i = 0; i < script.attributes.length; i++) {
+    var attribute = script.attributes.item(i);
+    attrs[attribute.name] = attribute.value;
+}
+attrs.url = location.href;
+attrs.origin = location.origin;
+attrs.pathname = location.pathname.substr(1).replace(/\.\w+$/, '');
+attrs.title = document.title;
+var descriptionMeta = document.querySelector("meta[name='description']");
+attrs.description = descriptionMeta ? descriptionMeta.content : '';
+var page = script.src.replace(/\/client(\.debug)?\.js(?:$|\?)/, '/utterances$1.html');
+var iframe = document.createElement('iframe');
+iframe.style.border = '0';
+iframe.style.width = '100%';
+iframe.setAttribute('allowtransparency', 'true');
+iframe.src = page + "?" + param(attrs);
+script.insertAdjacentElement('afterend', iframe);
+script.parentElement.removeChild(script);
+addEventListener('message', function (event) {
+    var data = event.data;
+    if (data && data.type === 'resize' && data.height) {
+        iframe.style.minHeight = data.height + "px";
+    }
+});
+
+}());
