8e0d5b4c003ba04ec9483c63474ef3127e8af994
Jeremy Danyow <jdanyow@gmail.com>
2017-05-02 07:06:01 -0700

    Updates

--- client.debug.js
+++ client.debug.js
@@ -23,7 +23,7 @@ attrs.pathname = location.pathname.substr(1).replace(/\.\w+$/, '');
 attrs.title = document.title;
 var descriptionMeta = document.querySelector("meta[name='description']");
 attrs.description = descriptionMeta ? descriptionMeta.content : '';
-document.head.insertAdjacentHTML('afterbegin', "<style>\n    .utterances {\n      position: relative;\n      width: 100%;\n    }\n    .utterances-frame {\n      position: absolute;\n      left: 0;\n      right: 0;\n      width: 100%;\n      height: 100%;\n      border: 0;\n    }\n  </style>");
+document.head.insertAdjacentHTML('afterbegin', "<style>\n    .utterances {\n      position: relative;\n      width: 100%;\n    }\n    .utterances-frame {\n      position: absolute;\n      left: 0;\n      right: 0;\n      width: 1px;\n      min-width: 100%;\n      max-width: 100%;\n      height: 100%;\n      border: 0;\n    }\n  </style>");
 var url = script.src.replace(/\/client(\.debug)?\.js(?:$|\?)/, '/utterances$1.html');
 script.insertAdjacentHTML('afterend', "<div class=\"utterances\">\n    <iframe class=\"utterances-frame\" scrolling=\"no\" src=\"" + url + "?" + param(attrs) + "\"></iframe>\n  </div>");
 var container = script.nextElementSibling;
