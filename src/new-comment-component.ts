576f8fce3f8a76c9e54e614ddddfd98ba78dc318
Jeremy Danyow <jdanyow@gmail.com>
2017-05-14 06:32:49 -0700

    IE fixes

--- src/new-comment-component.ts
+++ src/new-comment-component.ts
@@ -3,6 +3,7 @@ import { publishResize } from './bus';
 
 // tslint:disable-next-line:max-line-length
 const anonymousAvatar = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16" version="1.1"><path fill="rgb(179,179,179)" fill-rule="evenodd" d="M8 10.5L9 14H5l1-3.5L5.25 9h3.5L8 10.5zM10 6H4L2 7h10l-2-1zM9 2L7 3 5 2 4 5h6L9 2zm4.03 7.75L10 9l1 2-2 3h3.22c.45 0 .86-.31.97-.75l.56-2.28c.14-.53-.19-1.08-.72-1.22zM4 9l-3.03.75c-.53.14-.86.69-.72 1.22l.56 2.28c.11.44.52.75.97.75H5l-2-3 1-2z"></path></svg>`;
+// base64 encoding works in IE, Edge. UTF-8 does not.
 const anonymousAvatarUrl = `data:image/svg+xml;base64,${btoa(anonymousAvatar)}`;
 
 export class NewCommentComponent {
@@ -20,42 +21,27 @@ export class NewCommentComponent {
       <a class="avatar" target="_blank">
         <img height="44" width="44">
       </a>
-      <div class="comment">
+      <form class="comment" accept-charset="UTF-8" action="javascript:">
         <header class="comment-header">
           <strong>Join the discussion</strong>
         </header>
-        <form class="comment-body" id="comment-form" accept-charset="UTF-8" action="javascript:">
+        <div class="comment-body">
           <textarea placeholder="Leave a comment" aria-label="comment"></textarea>
-        </form>
+        </div>
         <footer class="comment-footer">
           <a class="text-link markdown-info" tabindex="-1" target="_blank"
              href="https://guides.github.com/features/mastering-markdown/">
             Styling with Markdown is supported
           </a>
-          <button class="btn btn-primary" form="comment-form" type="submit">Comment</button>
+          <button class="btn btn-primary" type="submit">Comment</button>
         </footer>
-      </div>`;
+      </form>`;
 
     this.setUser(user);
-  }
 
-  public setUser(user: User | null) {
-    this.user = user;
-    const avatarAnchor = this.element.firstElementChild as HTMLAnchorElement;
-    const avatar = avatarAnchor.firstElementChild as HTMLImageElement;
-    if (user) {
-      avatarAnchor.href = user.html_url;
-      avatar.alt = '@' + user.login;
-      avatar.src = user.avatar_url + '?v=3&s=88';
-    } else {
-      avatarAnchor.removeAttribute('href');
-      avatar.alt = '@anonymous';
-      avatar.src = anonymousAvatarUrl;
-    }
-
-    const form = avatarAnchor.nextElementSibling!.firstElementChild!.nextElementSibling as HTMLFormElement;
-    const textarea = form.firstElementChild as HTMLTextAreaElement;
-    const submitButton = form.nextElementSibling!.lastElementChild as HTMLButtonElement;
+    const form = this.element.firstElementChild!.nextElementSibling as HTMLFormElement;
+    const textarea = form!.firstElementChild!.nextElementSibling!.firstElementChild as HTMLTextAreaElement;
+    const submitButton = form!.lastElementChild!.lastElementChild as HTMLButtonElement;
 
     submitButton.textContent = user ? 'Comment' : 'Sign in to comment';
     submitButton.disabled = !!user;
@@ -73,7 +59,6 @@ export class NewCommentComponent {
     let submitting = false;
     form.addEventListener('submit', event => {
       event.preventDefault();
-
       if (submitting) {
         return;
       }
@@ -89,6 +74,21 @@ export class NewCommentComponent {
         submitButton.disabled = false;
       });
     });
+  }
+
+  public setUser(user: User | null) {
+    this.user = user;
+    const avatarAnchor = this.element.firstElementChild as HTMLAnchorElement;
+    const avatar = avatarAnchor.firstElementChild as HTMLImageElement;
+    if (user) {
+      avatarAnchor.href = user.html_url;
+      avatar.alt = '@' + user.login;
+      avatar.src = user.avatar_url + '?v=3&s=88';
+    } else {
+      avatarAnchor.removeAttribute('href');
+      avatar.alt = '@anonymous';
+      avatar.src = anonymousAvatarUrl;
+    }
   }
 
   public clear() {
