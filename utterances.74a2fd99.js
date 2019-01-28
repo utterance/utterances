eff320323f1747f8cd3249a67b03b95e6d149551
Jeremy Danyow <jdanyow@gmail.com>
2019-01-27 19:01:04 -0800

    Updates

--- utterances.74a2fd99.js
+++ utterances.74a2fd99.js
@@ -242,15 +242,151 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.login = login;
+exports.completeLogin = completeLogin;
 exports.token = void 0;
 
 var _utterancesApi = require("./utterances-api");
 
 var _deparam = require("./deparam");
 
-var authorizeUrl = _utterancesApi.UTTERANCES_API + "/authorize";
-var tokenUrl = _utterancesApi.UTTERANCES_API + "/token";
-var redirect_uri = location.origin + "/authorized.html";
+var __awaiter = void 0 && (void 0).__awaiter || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : new P(function (resolve) {
+        resolve(result.value);
+      }).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
 
 var Token = function () {
   function Token() {
@@ -286,29 +422,51 @@ var Token = function () {
 var token = new Token();
 exports.token = token;
 
-function login() {
-  window.open(authorizeUrl + "?" + (0, _deparam.param)({
+function login(redirect_uri) {
+  redirect_uri = location.origin + "/authorized.html?" + (0, _deparam.param)({
     redirect_uri: redirect_uri
-  }));
-  return new Promise(function (resolve) {
-    return window.notifyAuthorized = resolve;
-  }).then(function (search) {
-    return fetch(tokenUrl + search, {
-      mode: 'cors'
-    });
-  }).then(function (response) {
-    if (response.ok) {
-      return response.json();
-    }
+  });
+  window.open(_utterancesApi.UTTERANCES_API + "/authorize?" + (0, _deparam.param)({
+    redirect_uri: redirect_uri
+  }), '_top');
+}
 
-    return response.text().then(function (text) {
-      return Promise.reject("Error retrieving token:\n" + text);
+function completeLogin() {
+  return __awaiter(this, void 0, void 0, function () {
+    var _a, state, redirect_uri, tokenUrl, response, error, data;
+
+    return __generator(this, function (_b) {
+      switch (_b.label) {
+        case 0:
+          _a = (0, _deparam.deparam)(location.search.substr(1)), state = _a.state, redirect_uri = _a.redirect_uri;
+          tokenUrl = _utterancesApi.UTTERANCES_API + "/token?" + (0, _deparam.param)({
+            state: state
+          });
+          return [4, fetch(tokenUrl, {
+            mode: 'cors'
+          })];
+
+        case 1:
+          response = _b.sent();
+          if (!!response.ok) return [3, 3];
+          token.value = null;
+          return [4, response.text()];
+
+        case 2:
+          error = _b.sent();
+          document.body.textContent = error;
+          throw new Error(error);
+
+        case 3:
+          return [4, response.json()];
+
+        case 4:
+          data = _b.sent();
+          token.value = data;
+          location.href = redirect_uri;
+          return [2];
+      }
     });
-  }).then(function (t) {
-    token.value = t;
-  }, function (reason) {
-    token.value = null;
-    throw reason;
   });
 }
 },{"./utterances-api":"utterances-api.ts","./deparam":"deparam.ts"}],"encoding.ts":[function(require,module,exports) {
@@ -1288,17 +1446,7 @@ function bootstrap(issue, user) {
             return [2];
 
           case 5:
-            return [4, (0, _oauth.login)()];
-
-          case 6:
-            _a.sent();
-
-            return [4, (0, _github.loadUser)()];
-
-          case 7:
-            user = _a.sent();
-            timeline.setUser(user);
-            newCommentComponent.setUser(user);
+            (0, _oauth.login)(_pageAttributes.pageAttributes.url);
             return [2];
         }
       });
@@ -1318,7 +1466,7 @@ addEventListener('not-installed', function handleNotInstalled() {
 
 function assertOrigin() {
   return __awaiter(this, void 0, void 0, function () {
-    var origins, origin, owner, repo, url;
+    var origins, origin, owner, repo;
     return __generator(this, function (_a) {
       switch (_a.label) {
         case 0:
@@ -1326,7 +1474,7 @@ function assertOrigin() {
 
         case 1:
           origins = _a.sent().origins;
-          origin = _pageAttributes.pageAttributes.origin, owner = _pageAttributes.pageAttributes.owner, repo = _pageAttributes.pageAttributes.repo, url = _pageAttributes.pageAttributes.url;
+          origin = _pageAttributes.pageAttributes.origin, owner = _pageAttributes.pageAttributes.owner, repo = _pageAttributes.pageAttributes.repo;
 
           if (origins.indexOf(origin) !== -1) {
             return [2];
