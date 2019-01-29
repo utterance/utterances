782172997040063d5f715b237e957a77459019d1
Jeremy Danyow <jdanyow@gmail.com>
2019-01-28 19:37:26 -0800

    Updates

--- utterances.74a2fd99.js
+++ utterances.74a2fd99.js
@@ -241,7 +241,7 @@ exports.UTTERANCES_API = UTTERANCES_API;
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.login = login;
+exports.getLoginUrl = getLoginUrl;
 exports.completeLogin = completeLogin;
 exports.token = void 0;
 
@@ -422,13 +422,13 @@ var Token = function () {
 var token = new Token();
 exports.token = token;
 
-function login(redirect_uri) {
+function getLoginUrl(redirect_uri) {
   redirect_uri = location.origin + "/authorized.html?" + (0, _deparam.param)({
     redirect_uri: redirect_uri
   });
-  window.open(_utterancesApi.UTTERANCES_API + "/authorize?" + (0, _deparam.param)({
+  return _utterancesApi.UTTERANCES_API + "/authorize?" + (0, _deparam.param)({
     redirect_uri: redirect_uri
-  }), '_top');
+  });
 }
 
 function completeLogin() {
@@ -1031,6 +1031,8 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.NewCommentComponent = void 0;
 
+var _pageAttributes = require("./page-attributes");
+
 var _github = require("./github");
 
 var _measure = require("./measure");
@@ -1038,6 +1040,147 @@ var _measure = require("./measure");
 var _commentComponent = require("./comment-component");
 
 var _repoConfig = require("./repo-config");
+
+var _oauth = require("./oauth");
+
+var __awaiter = void 0 && (void 0).__awaiter || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : new P(function (resolve) {
+        resolve(result.value);
+      }).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
 
 var anonymousAvatar = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 14 16\" version=\"1.1\"><path fill=\"rgb(179,179,179)\" fill-rule=\"evenodd\" d=\"M8 10.5L9 14H5l1-3.5L5.25 9h3.5L8 10.5zM10 6H4L2 7h10l-2-1zM9 2L7 3 5 2 4 5h6L9 2zm4.03 7.75L10 9l1 2-2 3h3.22c.45 0 .86-.31.97-.75l.56-2.28c.14-.53-.19-1.08-.72-1.22zM4 9l-3.03.75c-.53.14-.86.69-.72 1.22l.56 2.28c.11.44.52.75.97.75H5l-2-3 1-2z\"></path></svg>";
 var anonymousAvatarUrl = "data:image/svg+xml;base64," + btoa(anonymousAvatar);
@@ -1080,32 +1223,37 @@ var NewCommentComponent = function () {
     };
 
     this.handleSubmit = function (event) {
-      event.preventDefault();
+      return __awaiter(_this, void 0, void 0, function () {
+        return __generator(this, function (_a) {
+          switch (_a.label) {
+            case 0:
+              event.preventDefault();
 
-      if (_this.submitting) {
-        return;
-      }
+              if (this.submitting) {
+                return [2];
+              }
 
-      _this.submitting = true;
+              this.submitting = true;
+              this.textarea.disabled = true;
+              this.submitButton.disabled = true;
+              return [4, this.submit(this.textarea.value).catch(function () {
+                return 0;
+              })];
 
-      if (_this.user) {
-        _this.textarea.disabled = true;
-        _this.submitButton.disabled = true;
-      }
+            case 1:
+              _a.sent();
 
-      _this.submit(_this.textarea.value).catch(function () {
-        return 0;
-      }).then(function () {
-        _this.submitting = false;
-        _this.textarea.disabled = !_this.user;
-        _this.textarea.value = '';
-        _this.submitButton.disabled = false;
-
-        _this.handleClick({
-          target: _this.form.querySelector('.tabnav-tab.tab-write')
+              this.submitting = false;
+              this.textarea.disabled = !this.user;
+              this.textarea.value = '';
+              this.submitButton.disabled = false;
+              this.handleClick({
+                target: this.form.querySelector('.tabnav-tab.tab-write')
+              });
+              this.preview.textContent = nothingToPreview;
+              return [2];
+          }
         });
-
-        _this.preview.textContent = nothingToPreview;
       });
     };
 
@@ -1140,13 +1288,14 @@ var NewCommentComponent = function () {
 
     this.element = document.createElement('article');
     this.element.classList.add('timeline-comment');
-    this.element.innerHTML = "\n      <a class=\"avatar\" target=\"_blank\" tabindex=\"-1\">\n        <img height=\"44\" width=\"44\">\n      </a>\n      <form class=\"comment\" accept-charset=\"UTF-8\" action=\"javascript:\">\n        <header class=\"new-comment-header\">\n          <nav class=\"tabnav-tabs\" role=\"tablist\">\n            <button type=\"button\" class=\"tabnav-tab tab-write selected\"\n                    role=\"tab\" aria-selected=\"true\">\n              Write\n            </button>\n            <button type=\"button\" class=\"tabnav-tab tab-preview\"\n                    role=\"tab\">\n              Preview\n            </button>\n          </nav>\n        </header>\n        <div class=\"comment-body\">\n          <textarea class=\"form-control\" placeholder=\"Leave a comment\" aria-label=\"comment\"></textarea>\n          <div class=\"markdown-body\" style=\"display: none\">\n            " + nothingToPreview + "\n          </div>\n        </div>\n        <footer class=\"comment-footer\">\n          <a class=\"text-link markdown-info\" tabindex=\"-1\" target=\"_blank\"\n             href=\"https://guides.github.com/features/mastering-markdown/\">\n            <svg class=\"octicon v-align-bottom\" viewBox=\"0 0 16 16\" version=\"1.1\"\n              width=\"16\" height=\"16\" aria-hidden=\"true\">\n              <path fill-rule=\"evenodd\" d=\"M14.85 3H1.15C.52 3 0 3.52 0 4.15v7.69C0 12.48.52 13 1.15\n                13h13.69c.64 0 1.15-.52 1.15-1.15v-7.7C16 3.52 15.48 3 14.85 3zM9 11H7V8L5.5 9.92 4\n                8v3H2V5h2l1.5 2L7 5h2v6zm2.99.5L9.5 8H11V5h2v3h1.5l-2.51 3.5z\">\n              </path>\n            </svg>\n            Styling with Markdown is supported\n          </a>\n          <button class=\"btn btn-primary\" type=\"submit\">Comment</button>\n        </footer>\n      </form>";
+    this.element.innerHTML = "\n      <a class=\"avatar\" target=\"_blank\" tabindex=\"-1\">\n        <img height=\"44\" width=\"44\">\n      </a>\n      <form class=\"comment\" accept-charset=\"UTF-8\" action=\"javascript:\">\n        <header class=\"new-comment-header\">\n          <nav class=\"tabnav-tabs\" role=\"tablist\">\n            <button type=\"button\" class=\"tabnav-tab tab-write selected\"\n                    role=\"tab\" aria-selected=\"true\">\n              Write\n            </button>\n            <button type=\"button\" class=\"tabnav-tab tab-preview\"\n                    role=\"tab\">\n              Preview\n            </button>\n          </nav>\n        </header>\n        <div class=\"comment-body\">\n          <textarea class=\"form-control\" placeholder=\"Leave a comment\" aria-label=\"comment\"></textarea>\n          <div class=\"markdown-body\" style=\"display: none\">\n            " + nothingToPreview + "\n          </div>\n        </div>\n        <footer class=\"comment-footer\">\n          <a class=\"text-link markdown-info\" tabindex=\"-1\" target=\"_blank\"\n             href=\"https://guides.github.com/features/mastering-markdown/\">\n            <svg class=\"octicon v-align-bottom\" viewBox=\"0 0 16 16\" version=\"1.1\"\n              width=\"16\" height=\"16\" aria-hidden=\"true\">\n              <path fill-rule=\"evenodd\" d=\"M14.85 3H1.15C.52 3 0 3.52 0 4.15v7.69C0 12.48.52 13 1.15\n                13h13.69c.64 0 1.15-.52 1.15-1.15v-7.7C16 3.52 15.48 3 14.85 3zM9 11H7V8L5.5 9.92 4\n                8v3H2V5h2l1.5 2L7 5h2v6zm2.99.5L9.5 8H11V5h2v3h1.5l-2.51 3.5z\">\n              </path>\n            </svg>\n            Styling with Markdown is supported\n          </a>\n          <button class=\"btn btn-primary\" type=\"submit\" hidden>Comment</button>\n          <a class=\"btn btn-primary\" href=\"" + (0, _oauth.getLoginUrl)(_pageAttributes.pageAttributes.url) + "\" target=\"_top\">Sign in to comment</a>\n        </footer>\n      </form>";
     this.avatarAnchor = this.element.firstElementChild;
     this.avatar = this.avatarAnchor.firstElementChild;
     this.form = this.avatarAnchor.nextElementSibling;
     this.textarea = this.form.firstElementChild.nextElementSibling.firstElementChild;
     this.preview = this.form.firstElementChild.nextElementSibling.lastElementChild;
-    this.submitButton = this.form.lastElementChild.lastElementChild;
+    this.signInAnchor = this.form.lastElementChild;
+    this.submitButton = this.signInAnchor.previousElementSibling;
     this.setUser(user);
     this.textarea.addEventListener('input', this.handleInput);
     this.form.addEventListener('submit', this.handleSubmit);
@@ -1157,8 +1306,8 @@ var NewCommentComponent = function () {
 
   NewCommentComponent.prototype.setUser = function (user) {
     this.user = user;
-    this.submitButton.textContent = user ? 'Comment' : 'Sign in to comment';
-    this.submitButton.disabled = !!user;
+    this.submitButton.hidden = !user;
+    this.signInAnchor.hidden = !!user;
 
     if (user) {
       this.avatarAnchor.href = user.html_url;
@@ -1194,7 +1343,7 @@ function handleTextAreaResize(textarea) {
 
   textarea.addEventListener('mousedown', track);
 }
-},{"./github":"github.ts","./measure":"measure.ts","./comment-component":"comment-component.ts","./repo-config":"repo-config.ts"}],"theme.ts":[function(require,module,exports) {
+},{"./page-attributes":"page-attributes.ts","./github":"github.ts","./measure":"measure.ts","./comment-component":"comment-component.ts","./repo-config":"repo-config.ts","./oauth":"oauth.ts"}],"theme.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -1228,8 +1377,6 @@ exports.assertOrigin = assertOrigin;
 var _pageAttributes = require("./page-attributes");
 
 var _github = require("./github");
-
-var _oauth = require("./oauth");
 
 var _timelineComponent = require("./timeline-component");
 
@@ -1422,7 +1569,6 @@ function bootstrap(issue, user) {
       return __generator(this, function (_a) {
         switch (_a.label) {
           case 0:
-            if (!user) return [3, 5];
             return [4, assertOrigin()];
 
           case 1:
@@ -1443,10 +1589,6 @@ function bootstrap(issue, user) {
             comment = _a.sent();
             timeline.appendComment(comment);
             newCommentComponent.clear();
-            return [2];
-
-          case 5:
-            (0, _oauth.login)(_pageAttributes.pageAttributes.url);
             return [2];
         }
       });
@@ -1489,5 +1631,5 @@ function assertOrigin() {
     });
   });
 }
-},{"./page-attributes":"page-attributes.ts","./github":"github.ts","./oauth":"oauth.ts","./timeline-component":"timeline-component.ts","./new-comment-component":"new-comment-component.ts","./measure":"measure.ts","./theme":"theme.ts","./repo-config":"repo-config.ts"}]},{},["utterances.ts"], null)
+},{"./page-attributes":"page-attributes.ts","./github":"github.ts","./timeline-component":"timeline-component.ts","./new-comment-component":"new-comment-component.ts","./measure":"measure.ts","./theme":"theme.ts","./repo-config":"repo-config.ts"}]},{},["utterances.ts"], null)
 //# sourceMappingURL=/utterances.74a2fd99.map
