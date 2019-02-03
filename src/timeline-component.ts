d6600112a24620d2c82d7f6cd768a1b5a407a70f
Jeremy Danyow <jdanyow@gmail.com>
2019-02-03 09:03:36 -0800

    pagination support (#115)

--- src/timeline-component.ts
+++ src/timeline-component.ts
@@ -42,21 +42,67 @@ export class TimelineComponent {
   public setIssue(issue: Issue | null) {
     this.issue = issue;
     if (issue) {
+      this.count = issue.comments;
       this.countAnchor.href = issue.html_url;
+      this.renderCount();
     } else {
       this.countAnchor.removeAttribute('href');
     }
   }
 
-  public appendComment(comment: IssueComment) {
+  public insertComment(comment: IssueComment, incrementCount: boolean) {
     const component = new CommentComponent(
       comment,
       this.user ? this.user.login : null);
-    this.timeline.push(component);
-    this.element.insertBefore(component.element, this.marker);
-    this.count++;
-    this.renderCount();
+
+    const index = this.timeline.findIndex(x => x.comment.id >= comment.id);
+    if (index === -1) {
+      this.timeline.push(component);
+      this.element.insertBefore(component.element, this.marker);
+    } else {
+      const next = this.timeline[index];
+      const remove = next.comment.id === comment.id;
+      this.element.insertBefore(component.element, next.element);
+      this.timeline.splice(index, remove ? 1 : 0, component);
+      if (remove) {
+        next.element.remove();
+      }
+    }
+
+    if (incrementCount) {
+      this.count++;
+      this.renderCount();
+    }
+
     scheduleMeasure();
+  }
+
+  public insertPageLoader(insertAfter: IssueComment, count: number, callback: () => void) {
+    const { element: insertAfterElement } = this.timeline.find(x => x.comment.id >= insertAfter.id)!;
+    insertAfterElement.insertAdjacentHTML('afterend', `
+      <div class="page-loader">
+        <div class="zigzag"></div>
+        <button type="button" class="btn btn-outline btn-large">
+          ${count} hidden items<br/>
+          <span>Load more...</span>
+        </button>
+      </div>
+    `);
+    const element = insertAfterElement.nextElementSibling!;
+    const button = element.lastElementChild! as HTMLButtonElement;
+    const statusSpan = button.lastElementChild!;
+    button.onclick = callback;
+
+    return {
+      setBusy() {
+        statusSpan.textContent = 'Loading...';
+        button.disabled = true;
+      },
+      remove() {
+        button.onclick = null;
+        element.remove();
+      }
+    };
   }
 
   private renderCount() {
