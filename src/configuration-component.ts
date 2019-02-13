3acb8672a8a9f3e8201a02dc83705f86e86a9584
Jaxson Van Doorn <woofers1tt@gmail.com>
2019-02-12 20:21:11 -0800

    Add GitHub dark orange theme (#122)
    Adds a new theme `GitHub Dark Orange` which is
    based on the existing GitHub Dark theme.
    
    The theme uses a mix of dark greys with orange accents
    on links and around currently focused elements.

--- src/configuration-component.ts
+++ src/configuration-component.ts
@@ -129,6 +129,7 @@ export class ConfigurationComponent {
       <select id="theme" class="form-select" value="github-light" aria-label="Theme">
         <option value="github-light">GitHub Light</option>
         <option value="github-dark">GitHub Dark</option>
+        <option value="github-dark-orange">GitHub Dark Orange</option>
       </select>
 
       <h3 id="heading-enable">Enable Utterances</h3>
