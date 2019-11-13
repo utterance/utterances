7b76e508bb95338bdd3db7f0e2514ef0c329251d
Jackson Mooring <jackson.mooring@gmail.com>
2019-11-13 11:53:35 -0600

    Enable sorting

--- src/timeline-component.ts
+++ src/timeline-component.ts
@@ -3,19 +3,19 @@ import { CommentComponent } from './comment-component';
 import { scheduleMeasure } from './measure';
 
 export class TimelineComponent {
-  public readonly element: HTMLElement;
-  private readonly timeline: CommentComponent[] = [];
-  private readonly countAnchor: HTMLAnchorElement;
-  private readonly marker: Node;
-  private count: number = 0;
+	public readonly element: HTMLElement;
+	private readonly timeline: CommentComponent[] = [];
+	private readonly countAnchor: HTMLAnchorElement;
+	private readonly marker: Node;
+	private count: number = 0;
 
-  constructor(
-    private user: User | null,
-    private issue: Issue | null
-  ) {
-    this.element = document.createElement('main');
-    this.element.classList.add('timeline');
-    this.element.innerHTML = `
+	constructor(
+		private user: User | null,
+		private issue: Issue | null
+	) {
+		this.element = document.createElement('main');
+		this.element.classList.add('timeline');
+		this.element.innerHTML = `
       <h1 class="timeline-header">
         <a class="text-link" target="_blank"></a>
         <em>
@@ -23,63 +23,68 @@ export class TimelineComponent {
           <a class="text-link" href="https://utteranc.es" target="_blank">utteranc.es</a>
         </em>
       </h1>`;
-    this.countAnchor = this.element.firstElementChild!.firstElementChild as HTMLAnchorElement;
-    this.marker = document.createComment('marker');
-    this.element.appendChild(this.marker);
-    this.setIssue(this.issue);
-    this.renderCount();
-  }
+		this.countAnchor = this.element.firstElementChild!.firstElementChild as HTMLAnchorElement;
+		this.marker = document.createComment('marker');
+		this.element.appendChild(this.marker);
+		this.setIssue(this.issue);
+		this.renderCount();
+	}
 
-  public setUser(user: User | null) {
-    this.user = user;
-    const login = user ? user.login : null;
-    for (let i = 0; i < this.timeline.length; i++) {
-      this.timeline[i].setCurrentUser(login);
-    }
-    scheduleMeasure();
-  }
+	public setUser(user: User | null) {
+		this.user = user;
+		const login = user ? user.login : null;
+		for (let i = 0; i < this.timeline.length; i++) {
+			this.timeline[i].setCurrentUser(login);
+		}
+		scheduleMeasure();
+	}
 
-  public setIssue(issue: Issue | null) {
-    this.issue = issue;
-    if (issue) {
-      this.count = issue.comments;
-      this.countAnchor.href = issue.html_url;
-      this.renderCount();
-    } else {
-      this.countAnchor.removeAttribute('href');
-    }
-  }
+	public setIssue(issue: Issue | null) {
+		this.issue = issue;
+		if (issue) {
+			this.count = issue.comments;
+			this.countAnchor.href = issue.html_url;
+			this.renderCount();
+		} else {
+			this.countAnchor.removeAttribute('href');
+		}
+	}
 
-  public insertComment(comment: IssueComment, incrementCount: boolean) {
-    const component = new CommentComponent(
-      comment,
-      this.user ? this.user.login : null);
+	public insertComment(comment: IssueComment, incrementCount: boolean, sortNew: boolean) {
+		const component = new CommentComponent(
+			comment,
+			this.user ? this.user.login : null
+		);
+		
+		const oldToNew = this.timeline.findIndex(x => x.comment.id >= comment.id);
+		const newToOld = this.timeline.findIndex(x => x.comment.id <= comment.id);
+		const index = sortNew ? newToOld : oldToNew;
 
-    const index = this.timeline.findIndex(x => x.comment.id >= comment.id);
-    if (index === -1) {
-      this.timeline.push(component);
-      this.element.insertBefore(component.element, this.marker);
-    } else {
-      const next = this.timeline[index];
-      const remove = next.comment.id === comment.id;
-      this.element.insertBefore(component.element, next.element);
-      this.timeline.splice(index, remove ? 1 : 0, component);
-      if (remove) {
-        next.element.remove();
-      }
-    }
+		if (index === -1) {
+			this.timeline.push(component);
+			this.element.insertBefore(component.element, this.marker);
+		} else {
+			const next = this.timeline[index];
+			const remove = next.comment.id === comment.id;
+			this.element.insertBefore(component.element, next.element);
+			this.timeline.splice(index, remove ? 1 : 0, component);
+			
+			if (remove) {
+				next.element.remove();
+			}
+		}
 
-    if (incrementCount) {
-      this.count++;
-      this.renderCount();
-    }
+		if (incrementCount) {
+			this.count++;
+			this.renderCount();
+		}
 
-    scheduleMeasure();
-  }
+		scheduleMeasure();
+	}
 
-  public insertPageLoader(insertAfter: IssueComment, count: number, callback: () => void) {
-    const { element: insertAfterElement } = this.timeline.find(x => x.comment.id >= insertAfter.id)!;
-    insertAfterElement.insertAdjacentHTML('afterend', `
+	public insertPageLoader(insertAfter: IssueComment, count: number, callback: () => void) {
+		const { element: insertAfterElement } = this.timeline.find(x => x.comment.id >= insertAfter.id)!;
+		insertAfterElement.insertAdjacentHTML('afterend', `
       <div class="page-loader">
         <div class="zigzag"></div>
         <button type="button" class="btn btn-outline btn-large">
@@ -88,24 +93,24 @@ export class TimelineComponent {
         </button>
       </div>
     `);
-    const element = insertAfterElement.nextElementSibling!;
-    const button = element.lastElementChild! as HTMLButtonElement;
-    const statusSpan = button.lastElementChild!;
-    button.onclick = callback;
+		const element = insertAfterElement.nextElementSibling!;
+		const button = element.lastElementChild! as HTMLButtonElement;
+		const statusSpan = button.lastElementChild!;
+		button.onclick = callback;
 
-    return {
-      setBusy() {
-        statusSpan.textContent = 'Loading...';
-        button.disabled = true;
-      },
-      remove() {
-        button.onclick = null;
-        element.remove();
-      }
-    };
-  }
+		return {
+			setBusy() {
+				statusSpan.textContent = 'Loading...';
+				button.disabled = true;
+			},
+			remove() {
+				button.onclick = null;
+				element.remove();
+			}
+		};
+	}
 
-  private renderCount() {
-    this.countAnchor.textContent = `${this.count} Comment${this.count === 1 ? '' : 's'}`;
-  }
+	private renderCount() {
+		this.countAnchor.textContent = `${this.count} Comment${this.count === 1 ? '' : 's'}`;
+	}
 }
--- src/utterances.ts
+++ src/utterances.ts
@@ -1,15 +1,15 @@
 import { pageAttributes as page } from './page-attributes';
 import {
-  Issue,
-  setRepoContext,
-  loadIssueByTerm,
-  loadIssueByNumber,
-  loadCommentsPage,
-  loadUser,
-  postComment,
-  createIssue,
-  PAGE_SIZE,
-  IssueComment
+	Issue,
+	setRepoContext,
+	loadIssueByTerm,
+	loadIssueByNumber,
+	loadCommentsPage,
+	loadUser,
+	postComment,
+	createIssue,
+	PAGE_SIZE,
+	IssueComment
 } from './github';
 import { TimelineComponent } from './timeline-component';
 import { NewCommentComponent } from './new-comment-component';
@@ -19,67 +19,76 @@ import { getRepoConfig } from './repo-config';
 import { loadToken } from './oauth';
 import { enableReactions } from './reactions';
 
+const sort = page.sort == 'new';
+
 setRepoContext(page);
 
 function loadIssue(): Promise<Issue | null> {
-  if (page.issueNumber !== null) {
-    return loadIssueByNumber(page.issueNumber);
-  }
-  return loadIssueByTerm(page.issueTerm as string);
+	if (page.issueNumber !== null) {
+		return loadIssueByNumber(page.issueNumber);
+	}
+	return loadIssueByTerm(page.issueTerm as string);
 }
 
 async function bootstrap() {
-  await loadToken();
-  // tslint:disable-next-line:prefer-const
-  let [issue, user] = await Promise.all([
-    loadIssue(),
-    loadUser(),
-    loadTheme(page.theme, page.origin)
-  ]);
+	await loadToken();
+	// tslint:disable-next-line:prefer-const
+	let [issue, user] = await Promise.all([
+		loadIssue(),
+		loadUser(),
+		loadTheme(page.theme, page.origin)
+	]);
 
-  startMeasuring(page.origin);
+	startMeasuring(page.origin);
 
-  const timeline = new TimelineComponent(user, issue);
-  document.body.appendChild(timeline.element);
+	const timeline = new TimelineComponent(user, issue);
+	document.body.appendChild(timeline.element);
 
-  if (issue && issue.comments > 0) {
-    renderComments(issue, timeline);
-  }
+	if (issue && issue.comments > 0) {
+		renderComments(issue, timeline);
+	}
 
-  scheduleMeasure();
+	scheduleMeasure();
 
-  if (issue && issue.locked) {
-    return;
-  }
+	if (issue && issue.locked) {
+		return;
+	}
 
-  enableReactions(!!user);
+	enableReactions(!!user);
 
-  const submit = async (markdown: string) => {
-    await assertOrigin();
-    if (!issue) {
-      issue = await createIssue(
-        page.issueTerm as string,
-        page.url,
-        page.title,
-        page.description,
-        page.label
-      );
-      timeline.setIssue(issue);
-    }
-    const comment = await postComment(issue.number, markdown);
-    timeline.insertComment(comment, true);
-    newCommentComponent.clear();
-  };
+	const submit = async (markdown: string) => {
+		await assertOrigin();
+		if (!issue) {
+			issue = await createIssue(
+				page.issueTerm as string,
+				page.url,
+				page.title,
+				page.description,
+				page.label
+			);
+			timeline.setIssue(issue);
+		}
+		const comment = await postComment(issue.number, markdown);
+		timeline.insertComment(comment, true, sort);
+		newCommentComponent.clear();
+	};
 
-  const newCommentComponent = new NewCommentComponent(user, submit);
-  timeline.element.appendChild(newCommentComponent.element);
+	const newCommentComponent = new NewCommentComponent(user, submit);
+	let firstbox = timeline.element.firstChild.nextSibling.nextSibling;
+	if (page.sort === 'new') {
+		timeline.element.insertBefore(newCommentComponent.element, firstbox);
+	} else {
+		timeline.element.append(newCommentComponent.element)
+	}
+
+
 }
 
 bootstrap();
 
 addEventListener('not-installed', function handleNotInstalled() {
-  removeEventListener('not-installed', handleNotInstalled);
-  document.querySelector('.timeline')!.insertAdjacentHTML('afterbegin', `
+	removeEventListener('not-installed', handleNotInstalled);
+	document.querySelector('.timeline')!.insertAdjacentHTML('afterbegin', `
   <div class="flash flash-error">
     Error: utterances is not installed on <code>${page.owner}/${page.repo}</code>.
     If you own this repo,
@@ -87,62 +96,63 @@ addEventListener('not-installed', function handleNotInstalled() {
     Read more about this change in
     <a href="https://github.com/utterance/utterances/pull/25" target="_top">the PR</a>.
   </div>`);
-  scheduleMeasure();
+	scheduleMeasure();
 });
 
 async function renderComments(issue: Issue, timeline: TimelineComponent) {
-  const renderPage = (page: IssueComment[]) => {
-    for (const comment of page) {
-      timeline.insertComment(comment, false);
-    }
-  };
 
-  const pageCount = Math.ceil(issue.comments / PAGE_SIZE);
-  // always load the first page.
-  const pageLoads = [loadCommentsPage(issue.number, 1)];
-  // if there are multiple pages, load the last page.
-  if (pageCount > 1) {
-    pageLoads.push(loadCommentsPage(issue.number, pageCount));
-  }
-  // if the last page is small, load the penultimate page.
-  if (pageCount > 2 && issue.comments % PAGE_SIZE < 3) {
-    pageLoads.push(loadCommentsPage(issue.number, pageCount - 1));
-  }
-  // await all loads to reduce jank.
-  const pages = await Promise.all(pageLoads);
-  for (const page of pages) {
-    renderPage(page);
-  }
-  // enable loading hidden pages.
-  let hiddenPageCount = pageCount - pageLoads.length;
-  let nextHiddenPage = 2;
-  const renderLoader = (afterPage: IssueComment[]) => {
-    if (hiddenPageCount === 0) {
-      return;
-    }
-    const load = async () => {
-      loader.setBusy();
-      const page = await loadCommentsPage(issue.number, nextHiddenPage);
-      loader.remove();
-      renderPage(page);
-      hiddenPageCount--;
-      nextHiddenPage++;
-      renderLoader(page);
-    };
-    const afterComment = afterPage.pop()!;
-    const loader = timeline.insertPageLoader(afterComment, hiddenPageCount * PAGE_SIZE, load);
-  };
-  renderLoader(pages[0]);
+	const renderPage = (page: IssueComment[]) => {
+		for (const comment of page) {
+			timeline.insertComment(comment, false, sort);
+		}
+	};
+
+	const pageCount = Math.ceil(issue.comments / PAGE_SIZE);
+	// always load the first page.
+	const pageLoads = [loadCommentsPage(issue.number, 1)];
+	// if there are multiple pages, load the last page.
+	if (pageCount > 1) {
+		pageLoads.push(loadCommentsPage(issue.number, pageCount));
+	}
+	// if the last page is small, load the penultimate page.
+	if (pageCount > 2 && issue.comments % PAGE_SIZE < 3) {
+		pageLoads.push(loadCommentsPage(issue.number, pageCount - 1));
+	}
+	// await all loads to reduce jank.
+	const pages = await Promise.all(pageLoads);
+	for (const page of pages) {
+		renderPage(page);
+	}
+	// enable loading hidden pages.
+	let hiddenPageCount = pageCount - pageLoads.length;
+	let nextHiddenPage = 2;
+	const renderLoader = (afterPage: IssueComment[]) => {
+		if (hiddenPageCount === 0) {
+			return;
+		}
+		const load = async () => {
+			loader.setBusy();
+			const page = await loadCommentsPage(issue.number, nextHiddenPage);
+			loader.remove();
+			renderPage(page);
+			hiddenPageCount--;
+			nextHiddenPage++;
+			renderLoader(page);
+		};
+		const afterComment = afterPage.pop()!;
+		const loader = timeline.insertPageLoader(afterComment, hiddenPageCount * PAGE_SIZE, load);
+	};
+	renderLoader(pages[0]);
 }
 
 export async function assertOrigin() {
-  const { origins } = await getRepoConfig();
-  const { origin, owner, repo } = page;
-  if (origins.indexOf(origin) !== -1) {
-    return;
-  }
+	const { origins } = await getRepoConfig();
+	const { origin, owner, repo } = page;
+	if (origins.indexOf(origin) !== -1) {
+		return;
+	}
 
-  document.querySelector('.timeline')!.lastElementChild!.insertAdjacentHTML('beforebegin', `
+	document.querySelector('.timeline')!.lastElementChild!.insertAdjacentHTML('beforebegin', `
   <div class="flash flash-error flash-not-installed">
     Error: <code>${origin}</code> is not permitted to post to <code>${owner}/${repo}</code>.
     Confirm this is the correct repo for this site's comments. If you own this repo,
@@ -153,6 +163,6 @@ export async function assertOrigin() {
     Suggested configuration:<br/>
     <pre><code>${JSON.stringify({ origins: [origin] }, null, 2)}</code></pre>
   </div>`);
-  scheduleMeasure();
-  throw new Error('Origin not permitted.');
+	scheduleMeasure();
+	throw new Error('Origin not permitted.');
 }
