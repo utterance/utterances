35d38ab0e4d31ba347d014f6811bdbd3e0a1e4c8
Jeremy Danyow <jdanyow@gmail.com>
2020-05-09 20:04:06 -0700

    Add prefers-color-scheme theme

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import { param, deparam } from './deparam';
 import { ResizeMessage } from './measure';
+import { preferredThemeId, preferredTheme } from './preferred-theme';
 
 // slice access token from query string
 const params = deparam(location.search.substr(1));
@@ -25,6 +26,9 @@ const attrs: Record<string, string> = {};
 for (let i = 0; i < script.attributes.length; i++) {
   const attribute = script.attributes.item(i)!;
   attrs[attribute.name.replace(/^data-/, '')] = attribute.value; // permit using data-theme instead of theme.
+}
+if (attrs.theme === preferredThemeId) {
+  attrs.theme = preferredTheme;
 }
 
 // gather page attributes
