3c79187fcb1ad6daf5d5caf7398f952f0a5a4709
李鹏坤-执手对影成双 <mail@lipk.org>
2021-06-10 17:21:51 +0800

    Fix #540

--- src/utterances.ts
+++ src/utterances.ts
@@ -105,7 +105,8 @@ async function renderComments(issue: Issue, timeline: TimelineComponent) {
     pageLoads.push(loadCommentsPage(issue.number, pageCount));
   }
   // if the last page is small, load the penultimate page.
-  if (pageCount > 2 && issue.comments % PAGE_SIZE < 3) {
+  if (pageCount > 2 && issue.comments % PAGE_SIZE < 3 &&
+    issue.comments % PAGE_SIZE !== 0) {
     pageLoads.push(loadCommentsPage(issue.number, pageCount - 1));
   }
   // await all loads to reduce jank.
